aef2a3547c1f1f6ec58629c6eb5d57eb4e88b732
Alexandre Souza <alexsz1609@gmail.com>
2025-08-12 17:21:16 -0300

    Project created

---
+++ src/test/java/com/nelioalves/workshop_springboot3_jpa/WorkshopSpringboot3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.workshop_springboot3_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
